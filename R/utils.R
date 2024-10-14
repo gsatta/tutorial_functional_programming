671007887f572c1082b90a77772e82547013f6ba
gsatta <g.satta8@phd.uniss.it>
2024-10-14 14:50:32 +0200

    Finish organizing functions

---
+++ R/utils.R
@@ -0,0 +1,17 @@
+
+
+# FUNCTION: convert girth from inches to centimetersconvert_in_to_cm <- function(x) {
+convert_in_to_cm <- function(x) {
+    x * 2.54
+}
+
+
+# FUNCTION: convert height from feet to meters
+convert_ft_to_m <- function(x) {
+    x * .3048
+}
+
+# FUNCTION: calculate volume in cubic meters
+calculate_volume <- function(diameter, height) {
+    pi / 4 * (diameter/100)^2 * height
+}
