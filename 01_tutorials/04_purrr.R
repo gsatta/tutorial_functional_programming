a058a4819bcf0c46cb9639951622699dcef7b3d6
gsatta <g.satta8@phd.uniss.it>
2024-10-14 17:19:25 +0200

    finished puur section

--- 01_tutorials/04_purrr.R
+++ 01_tutorials/04_purrr.R
@@ -26,10 +26,19 @@ objects_list <- list("peach", "pear", "cherry", "strawberry", "blackberry")
 ## 3.1. Using for loop ---------------------------
 
 ## Create an empty list
+message_list <- list()
 
+for (i in 1:length(objects_list)) {
+    message_list[[i]] <- str_glue("This is a {objects_list[[i]]}")
+}
 
 ## 3.2. Using functional programming -------------
 
+message_list <-  map(
+    objects_list, 
+    \(fruit) str_glue("This fruit is a {fruit}")
+) |> 
+    as.character()
 
 # 4. Model by species -----------------------------------------------------
 
@@ -44,24 +53,50 @@ species_vec <- unique(iris_tbl$Species)
 filtered_lst <- list()
 
 ## Iterate to filter each species
-
+for (species in 1:length(species_vec)) {
+    ## Filter the observations
+    iris_filtered <- iris_tbl |> 
+        filter(
+            Species == species_vec[species]
+        )
+    ## Calculate linear model
+    iris_filtered_lm <-  lm(Petal.Length ~ Sepal.Length, data = iris_filtered)
+    # Get summary
+    filtered_lst[[species]] <- summary(iris_filtered_lm)
+}
 
 ## 4.2. Using functional programming --------------
 
 ## Create a function
-
+claculate_iris_lm <-  function(data) {
+    ## Calculate linear model
+        lm(Petal.Length ~ Sepal.Length, data = iris_filtered) |> 
+        summary()
+}
 
 ## Create list
-
-
+iris_species_list <-  iris_tbl |> 
+    split(iris_tbl$Species)
+ 
 ## Iterate over the list
-
+map(
+    iris_species_list,
+    claculate_iris_lm
+)
 
 ## 4.3. In one step ---------------------------------
-
+iris_tbl |> 
+    split(iris_tbl$Species) |> 
+    map(claculate_iris_lm)
 
 ## 4.4. Using anonymous function --------------------
 
+iris_tbl |> 
+    split(iris_tbl$Species) |> 
+    map(
+        \(data) lm(Petal.Length ~ Sepal.Length, data = data) |> 
+            summary()
+    )
 
 # 5. Iterating 2 inputs --------------------------------------------------
 
@@ -70,21 +105,59 @@ filtered_lst <- list()
 ## Sum over the lists
 fruits <- list("peach", "pear", "cherry", "strawberry", "blackberry")
 colors <- list("orange", "green", "red", "red", "black")
+rounded <-  list("rounded", "not rounded", "rounded", "not rounded", "rounded")
 
 ## Iterate to create the sentence
+map2(
+    .x = fruits,
+    .y = colors, 
+    \(fruit, color) str_glue("The color of {fruit} is {color}")
+) |> as.character()
 
+## Iterate to create the sentence over 3 or more lists/vectors
+pmap(
+    .l = list(
+        fruits, 
+        colors,
+        rounded
+    ), 
+    \(fruit, color, rounded) str_glue("The color of {fruit} is {color} and {rounded}")
+)
 
 ## 5.2. A bigger example --------------------------
 
 ## Create a grid of parameters
-
+parms_tbl <- expand_grid(
+    ntree = c(100, 200, 500, 1000), 
+    mtry = 1:4
+)
 
 ## Create a Random Forest model for each parameter
-
+iris_rf_list <- map2(
+    .x = parms_tbl$ntree,
+    .y = parms_tbl$mtry,
+    \(ntree, mtry) ranger(
+        formul    = Sepal.Length ~.,
+        data      = iris_tbl,
+        num.trees = ntree,
+        mtry      = mtry
+    )
+)
 
 ## Extract r.squared
-
+map_dbl(
+    iris_rf_list,
+    \(rf_model) rf_model$r.squared
+)
 
 ## Add as a new column
-
+parms_tbl |> 
+    mutate(
+        rsq = map_dbl(
+            iris_rf_list,
+            \(rf_model) rf_model$r.squared
+        )
+        
+    ) |> 
+    arrange(desc(rsq))
 
